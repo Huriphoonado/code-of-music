f476e99b2033008de33cd6fc4234a244f272f339
Willie Payne <williepain@gmail.com>
2018-10-04 12:26:26 -0400

    Added sound, some basic animations and controls

--- projects/Timbre/blocks.js
+++ projects/Timbre/blocks.js
@@ -5,7 +5,7 @@ function Block(x, b_width, min_mouse) {
     this.x = x;
     this.b_width = b_width;
 
-    this.min_height = height / 10;
+    this.min_height = min_mouse;
     this.max_height = height - (height / 10);
 
     // How tall now, and how tall could it get?
@@ -16,28 +16,39 @@ function Block(x, b_width, min_mouse) {
     this.min_mouse = min_mouse;
     this.we_just_randomized = true;
 
-    this.update = function() {
-        this.updateSize();
+    this.shake_amount = 10;
+
+    this.color = 50;
+
+    this.update = function(active) {
+        this.updateSize(active);
     }
 
     this.show = function() {
         // May need to play with how its size changes
         // Potential fix for testing - set at height/2
         this.updateColor();
-        rect(this.x, height - this.b_height, this.b_width, this.b_height, 15, 15, 0, 0);
+        rect(this.x, height - this.b_height, this.b_width, this.b_height, 10, 10, 0, 0);
     }
 
-    this.updateSize = function() {
+    this.updateSize = function(active) {
         this.random_chance();
         this.random_size_on_switch();
-        let destinatation_height = this.calculate_height();
-        this.b_height = lerp(this.b_height, destinatation_height, lerp_val);
+        this.update_shake();
+
+        let destination_height = this.calculate_height();
+
+        let rand_shake = floor(this.shake_amount / 2);
+        if (active) { rand_shake = floor(random(this.shake_amount)); }
+
+        this.b_height = lerp(this.b_height, destination_height + rand_shake, lerp_val);
     }
 
     this.updateColor = function() {
-        let color = map(this.b_height, 0, height, 50, 200);
-        stroke(color);
-        fill(color);
+        let destination_color = map(this.b_height, 0, height, 50, 200);
+        this.color = lerp(this.color, destination_color, 0.1);
+        stroke(this.color);
+        fill(this.color);
     }
 
     // If this happens randomly - it may lead to occasionally shifting blocks
@@ -45,7 +56,6 @@ function Block(x, b_width, min_mouse) {
         if (this.inverted_y() <= this.min_mouse && !(this.we_just_randomized)) {
             this.potential = floor(random(this.min_height, this.max_height));
             this.we_just_randomized = true;
-            console.log(this.potential);
         }
         else if (this.inverted_y() > this.min_mouse) {
             this.we_just_randomized = false;
@@ -53,7 +63,7 @@ function Block(x, b_width, min_mouse) {
     }
 
     this.calculate_height = function() {
-        return map(this.inverted_y(), 0, height, 0, this.potential, true);
+        return map(this.inverted_y(), 0, height, this.min_height, this.potential, true);
     }
 
     this.random_chance = function() {
@@ -62,6 +72,10 @@ function Block(x, b_width, min_mouse) {
         if (random_num <= random_chance) {
             this.potential = floor(random(this.min_height, this.max_height));
         }
+    }
+
+    this.update_shake = function() {
+        this.shake_amount = floor(map(this.inverted_y(), 0, height, 5, 100));
     }
 
     // Invert y coordinates so that the bottom of the screen is zero
@@ -74,9 +88,12 @@ function Block(x, b_width, min_mouse) {
 
 function Blocks(num_blocks, min_mouse) {
     this.blocks = [];
+
+    // This algorithm prevents overlap, but feels a little complicated?
     for (let i = 0; i<num_blocks; i++) {
-        let new_width = width/num_blocks;
-        let new_pos = floor(map(i, 0, num_blocks, 0, width));
+        let new_width = (width/num_blocks) - 1;
+        let new_pos = (i * new_width) + i;
+        // let new_pos = ceil(map(i, 0, num_blocks, 0, width));
         this.blocks.push(new Block(new_pos, new_width, min_mouse));
     }
 
@@ -86,9 +103,9 @@ function Blocks(num_blocks, min_mouse) {
         });
     }
 
-    this.update = function() {
+    this.update = function(active) {
         this.blocks.forEach(function(block) {
-            block.update();
+            block.update(active);
         });
     }
 }
--- projects/Timbre/sketch.js
+++ projects/Timbre/sketch.js
@@ -4,16 +4,128 @@
 const c_width = 600; // Window Size params
 const c_height = 400;
 
+let bcg_col = {
+    true: {r: 220, g: 220, b: 220},
+    false: {r: 40, g: 40, b: 40},
+    curr: {r: 220,g: 220,b: 220}
+}
+
+let active = false;
+
+// When the mouse goes below this height, it acts as a button
+const trigger_height = c_height/10;
+
 let blocks;
+let oscillators;
+
+let num_oscillators = 6;
+
+let tremolo = new Tone.Tremolo(8, 0.5).start();
+tremolo.type = 'triangle';
+tremolo.spread = 10;
+let reverb = new Tone.JCReverb(0.8);
+let reverb_vol = new Tone.Volume(-8);
+let master_vol = new Tone.Volume(0);
 
 function setup() {
     createCanvas(c_width, c_height);
-    background(220);
-    blocks = new Blocks(60, height/10);
+    set_background();
+    oscillators = build_oscillators(num_oscillators, tremolo, reverb, reverb_vol, master_vol);
+    blocks = new Blocks(60, trigger_height);
 }
 
 function draw() {
-    background(220);
-    blocks.update();
+    set_background();
+    blocks.update(active);
     blocks.show();
+
+    map_mouse_position();
+}
+
+function set_background() {
+    let lerper = 0.05;
+    bcg_col.curr.r = lerp(bcg_col.curr.r, bcg_col[active].r, lerper);
+    bcg_col.curr.g = lerp(bcg_col.curr.g, bcg_col[active].g, lerper);
+    bcg_col.curr.b = lerp(bcg_col.curr.b, bcg_col[active].b, lerper);
+    background(bcg_col.curr.r, bcg_col.curr.g, bcg_col.curr.b);
+}
+
+function mousePressed() {
+    active = true;
+    tremolo.start();
+    oscillators.forEach(function(o) {
+        o.oscillator.triggerAttack(o.pitch);
+    });
+
+    return false;
+}
+
+function mouseReleased() {
+    active = false;
+    oscillators.forEach(function(o) {
+        o.oscillator.triggerRelease();
+    });
+
+    return false;
+}
+
+function map_mouse_position() {
+    // console.log(tremolo.depth);
+    let i_y = inverted_y();
+
+    // Tremolo Mapping
+    let t_control = map(i_y, 0, height, 0.1, 1.0, true)
+    tremolo.depth.rampTo(t_control, 0.1);
+
+    // Volume Mapping
+    let m_vol_control = map(i_y, 0, height, -20, 0, true);
+    master_vol.volume.rampTo(m_vol_control, 0.1);
+
+    // Individual Instrument Mapping
+    let o_vol_control = map(i_y, 0, height, -40, -12, true)
+    oscillators[0].panner.volume.rampTo(o_vol_control, 0.1);
+    oscillators[oscillators.length-1].panner.volume.rampTo(o_vol_control, 0.1);
+
+}
+
+function inverted_y() {
+    return height - mouseY;
+}
+
+//
+function build_oscillators(num_oscillators, tremolo, reverb, reverb_vol, master_vol) {
+    let new_oscillators = [];
+    let pan_max = 0.8;
+
+    for (i=0; i<num_oscillators; i++) {
+        let new_oscillator = build_oscillator();
+        let new_panner = new Tone.PanVol(map(i, 0, num_oscillators, -0.8, 0.8), -12);
+
+        new_oscillator.chain(new_panner, tremolo, reverb, reverb_vol, master_vol, Tone.Master);
+        new_oscillator.chain(new_panner, tremolo, master_vol, Tone.Master);
+
+        new_oscillators.push({
+            oscillator: new_oscillator,
+            panner: new_panner,
+            pitch: 'C' + i
+        });
+    }
+
+    return new_oscillators
+}
+
+// Swap out code here for new oscillator.
+// Must return some Tone synth object
+function build_oscillator() {
+    return new Tone.Synth({
+        'oscillator': {
+            type: 'triangle'
+        },
+        'envelope': {
+            attack: 1,
+            decay: 0.0,
+            sustain: 1,
+            release: 1
+        }
+    });
 }
